eb3de23a58ad5c4949729205bda92cf005f6e653
Xa9aX ツ <mishradiganta91@gmail.com>
2023-07-03 02:16:53 -0400

    update scripts

--- scripts/linear_probing.sh
+++ scripts/linear_probing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=1shot_baseline
+#SBATCH --job-name=LinearProbing
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -7,8 +7,8 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --partition=long
-#SBATCH --output=/home/mila/d/diganta.misra/projects/prune_train/logs/1shot_baseline.out
-#SBATCH --error=/home/mila/d/diganta.misra/projects/prune_train/logs/1shot_baseline.err
+#SBATCH --output=/home/mila/d/diganta.misra/projects/ILM-VP/logs/LP.out
+#SBATCH --error=/home/mila/d/diganta.misra/projects/ILM-VP/logs/LP.err
 
 module load anaconda/3
 
@@ -18,12 +18,13 @@ wandb login bd67cef57b7227730fe3edf96e11d954558a9d0d
 
 ulimit -Sn $(ulimit -Hn)
 
-python3 experiments/cnn/linear_probing.py \
+python3 /home/mila/d/diganta.misra/projects/ILM-VP/experiments/cnn/linear_probing.py \
         --network LT \
         --dataset cifar10 \
-        --pretrained_dir /data/jaygala/ILM-VP/artifacts/ImageNetCheckpoint_LT \
-        --results_path /data/jaygala/ILM-VP/results \
+        --pretrained_dir /home/mila/d/diganta.misra/scratch/ImageNetCheckpoint \
+        --results_path /home/mila/d/diganta.misra/scratch/vp_results \
         --sparsity 9 \
+        --seed 1 \
         --train_data_fraction 0.5 \
         --wandb \
         --run_name linear_probing \
