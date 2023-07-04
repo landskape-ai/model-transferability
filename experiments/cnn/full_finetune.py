7e5caa7433d395c199e4b134dd858e271d3b635f
Bharat <58414297+Bharat-Runwal@users.noreply.github.com>
2023-07-04 18:32:26 +0530

    Fix Sparsity [Finetune]

--- experiments/cnn/full_finetune.py
+++ experiments/cnn/full_finetune.py
@@ -9,19 +9,79 @@ from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 from torchvision.models import ResNet18_Weights, resnet18
 from tqdm import tqdm
+import torchvision
+import torch.nn as nn
 
 sys.path.append(".")
-from cfg import *
-from masking_utils import get_wrapped_model
 
 from data import IMAGENETNORMALIZE, prepare_additive_data
 from tools.misc import gen_folder_name, set_seed
 
+
+
+import wandb as wb
+
+
+def wandb_setup(
+    args
+):
+    return wb.init(
+        config =args,
+        project="Reprogram-Sparse",
+        dir="./wandb",
+    )
+
+def check_sparsity(model, conv1=True):
+    
+    sum_list = 0
+    zero_sum = 0
+
+    for name,m in model.named_modules():
+        if isinstance(m, nn.Conv2d):
+            if name == 'conv1':
+                if conv1:
+                    sum_list = sum_list+float(m.weight.nelement())
+                    zero_sum = zero_sum+float(torch.sum(m.weight == 0))    
+                else:
+                    print('skip conv1 for sparsity checking')
+            else:
+                sum_list = sum_list+float(m.weight.nelement())
+                zero_sum = zero_sum+float(torch.sum(m.weight == 0))  
+
+    print('* remain weight = ', 100*(1-zero_sum/sum_list),'%')
+    
+    return 100*(1-zero_sum/sum_list)
+
+
+def get_pruned_model(args):
+
+    pretrained = args.pretrained
+  
+    mask_dir = args.mask_dir
+    
+    current_mask_weight = torch.load(mask_dir)
+    curr_weight = torch.load(pretrained)
+
+
+    new_weights = {}
+    for name in current_mask_weight.keys():
+        name_ = name.replace("model.","")
+        new_weights[str(name_)] = current_mask_weight[str(name)] * curr_weight[str(name)]
+
+    for k in curr_weight.keys():
+        if str(k) not in new_weights.keys():
+            # print(k)
+            k_ = k.replace("model.","")
+
+            new_weights[k_] = curr_weight[k]
+
+    return new_weights
+
+
+
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument(
-        "--network", choices=["resnet18", "resnet50", "instagram"], required=True
-    )
+    p.add_argument('--network', choices=["LT", "rigL", "acdc", "STR", "dense"], default="LT")
     p.add_argument("--seed", type=int, default=7)
     p.add_argument(
         "--dataset",
@@ -42,15 +102,22 @@ if __name__ == "__main__":
         ],
         required=True,
     )
+    p.add_argument('--results_path',type = str, default = "/reprogram_new" )
+    p.add_argument("--mask_dir", type=str, default="/ImageNetCheckpoint/resnet50_dyn4_9_mask.pth")
+    p.add_argument("--pretrained", type=str, default="/ImageNetCheckpoint/resnet50_dyn4_9_checkpoint.pth")
     p.add_argument("--epoch", type=int, default=200)
     p.add_argument("--lr", type=float, default=0.01)
     args = p.parse_args()
 
+    wb_logger = wandb_setup(args)
+
+
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     set_seed(args.seed)
-
+    
     exp = f"cnn/full_finetuning"
-    save_path = os.path.join(results_path, exp, gen_folder_name(args))
+    save_path = os.path.join(args.results_path, exp, gen_folder_name(args))
+    data_path  =os.path.join(args.results_path,"data")
     preprocess = transforms.Compose(
         [
             transforms.Resize((256, 256)),
@@ -68,24 +135,40 @@ if __name__ == "__main__":
     )
 
     # Network
-    if args.network == "resnet18":
-        from torchvision.models import ResNet18_Weights, resnet18
-
-        network = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1).to(device)
-    elif args.network == "resnet50":
-        from torchvision.models import ResNet50_Weights, resnet50
-
+    if args.network == "dense":
+        from torchvision.models import resnet50, ResNet50_Weights
         network = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2).to(device)
-    elif args.network == "instagram":
-        from torch import hub
-
-        network = hub.load("facebookresearch/WSL-Images", "resnext101_32x8d_wsl").to(
-            device
-        )
+    elif args.network == "LT":
+        network = torchvision.models.__dict__["resnet50"](pretrained=(False)).to("cpu")
+        new_dict = get_pruned_model(args)
+        # network = network.to(device)
+        network.load_state_dict(new_dict)
+    elif args.network == "rigL":
+        network = torchvision.models.__dict__["resnet50"](pretrained=(False))
+        new_dict = get_pruned_model(args)
+        network = network.to(device)
+        network.load_state_dict(new_dict)
+    elif args.network == "acdc":
+        network = torchvision.models.__dict__["resnet50"](pretrained=(False))
+        new_dict = get_pruned_model(args)
+        network = network.to(device)
+        network.load_state_dict(new_dict)
+    elif args.network == "STR":
+        network = torchvision.models.__dict__["resnet50"](pretrained=(False))
+        new_dict = get_pruned_model(args)
+        network = network.to(device)
+        network.load_state_dict(new_dict)
     else:
         raise NotImplementedError(f"{args.network} is not supported")
-    network = get_wrapped_model(network)
+  
+
+    mask_dir = args.mask_dir
+    current_mask_weight = torch.load(mask_dir)
+
+    print("Check Sparsity Before Training : ")
+    check_sparsity(network)
     network.requires_grad_(True)
+
     network = network.to(device)
 
     # Optimizer
@@ -121,16 +204,33 @@ if __name__ == "__main__":
                 fx = network(x)
                 loss = F.cross_entropy(fx, y, reduction="mean")
             scaler.scale(loss).backward()
+
+
+            # Apply mask to param_data and gradients to preserve pruned connections
+            for name, param in network.named_parameters():
+                name = "model."+name
+                if name in current_mask_weight:
+                    param.data *= current_mask_weight[name].to(device)  
+                    param.grad *= current_mask_weight[name].to(device)  
+            #  END OF APPLYING MASKS 
+
             scaler.step(optimizer)
             scaler.update()
             total_num += y.size(0)
             true_num += torch.argmax(fx, 1).eq(y).float().sum().item()
             loss_sum += loss.item() * fx.size(0)
             pbar.set_postfix_str(f"Acc {100*true_num/total_num:.2f}%")
+
+
+       
+       
         scheduler.step()
         logger.add_scalar("train/acc", true_num / total_num, epoch)
         logger.add_scalar("train/loss", loss_sum / total_num, epoch)
-
+        wb_logger.log({"Train-Loss":loss_sum/total_num})
+        wb_logger.log({"Train-ACC":true_num/total_num})
+        wb_logger.log({"Sparsity":check_sparsity(network, False)})
+        
         # Test
         network.eval()
         total_num = 0
@@ -150,7 +250,7 @@ if __name__ == "__main__":
             acc = true_num / total_num
             pbar.set_postfix_str(f"Acc {100*acc:.2f}%")
         logger.add_scalar("test/acc", acc, epoch)
-
+        wb_logger.log({"Test-ACC":acc})
         # Save CKPT
         state_dict = {
             "network_dict": network.state_dict(),
@@ -162,4 +262,5 @@ if __name__ == "__main__":
             best_acc = acc
             state_dict["best_acc"] = best_acc
             torch.save(state_dict, os.path.join(save_path, "best.pth"))
+        check_sparsity(network)
         torch.save(state_dict, os.path.join(save_path, "ckpt.pth"))
