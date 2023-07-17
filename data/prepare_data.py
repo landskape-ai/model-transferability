3c757f9f1fb07ed2f5ebd9e346322b6aecd98102
Xa9aX ツ <mishradiganta91@gmail.com>
2023-07-17 16:44:23 -0400

    update

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -13,180 +13,6 @@ from .const import GTSRB_LABEL_MAP, IMAGENETNORMALIZE
 from .dataset_lmdb import COOPLMDBDataset
 
 
-def refine_classnames(class_names):
-    for i, class_name in enumerate(class_names):
-        class_names[i] = class_name.lower().replace("_", " ").replace("-", " ")
-    return class_names
-
-
-def get_class_names_from_split(root):
-    with open(os.path.join(root, "split.json")) as f:
-        split = json.load(f)["test"]
-    idx_to_class = OrderedDict(sorted({s[-2]: s[-1] for s in split}.items()))
-    return list(idx_to_class.values())
-
-
-def prepare_expansive_data(args, dataset, data_path):
-    data_path = os.path.join(data_path, dataset)
-    if dataset == "cifar10":
-        preprocess = transforms.Compose(
-            [
-                transforms.ToTensor(),
-            ]
-        )
-        train_data = datasets.CIFAR10(
-            root=data_path, train=True, download=True, transform=preprocess
-        )
-        test_data = datasets.CIFAR10(
-            root=data_path, train=False, download=True, transform=preprocess
-        )
-        train_data = get_data_splits(args, train_data)
-        loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=2),
-            "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
-        }
-        configs = {
-            "class_names": refine_classnames(test_data.classes),
-            "mask": np.zeros((32, 32)),
-        }
-    elif dataset == "cifar100":
-        preprocess = transforms.Compose(
-            [
-                transforms.ToTensor(),
-            ]
-        )
-        train_data = datasets.CIFAR100(
-            root=data_path, train=True, download=True, transform=preprocess
-        )
-        test_data = datasets.CIFAR100(
-            root=data_path, train=False, download=True, transform=preprocess
-        )
-        train_data = get_data_splits(args, train_data)
-        loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=2),
-            "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
-        }
-        configs = {
-            "class_names": refine_classnames(test_data.classes),
-            "mask": np.zeros((32, 32)),
-        }
-    elif dataset == "gtsrb":
-        preprocess = transforms.Compose(
-            [
-                transforms.Resize((32, 32)),
-                transforms.ToTensor(),
-            ]
-        )
-        train_data = datasets.GTSRB(
-            root=data_path, split="train", download=True, transform=preprocess
-        )
-        test_data = datasets.GTSRB(
-            root=data_path, split="test", download=True, transform=preprocess
-        )
-        train_data = get_data_splits(args, train_data)
-        loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=2),
-            "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
-        }
-        configs = {
-            "class_names": refine_classnames(list(GTSRB_LABEL_MAP.values())),
-            "mask": np.zeros((32, 32)),
-        }
-    elif dataset == "svhn":
-        preprocess = transforms.Compose(
-            [
-                transforms.ToTensor(),
-            ]
-        )
-        train_data = datasets.SVHN(
-            root=data_path, split="train", download=True, transform=preprocess
-        )
-        test_data = datasets.SVHN(
-            root=data_path, split="test", download=True, transform=preprocess
-        )
-        train_data = get_data_splits(args, train_data)
-        loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=2),
-            "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
-        }
-        configs = {
-            "class_names": [f"{i}" for i in range(10)],
-            "mask": np.zeros((32, 32)),
-        }
-    elif dataset == "abide":
-        preprocess = transforms.ToTensor()
-        D = ABIDE(root=data_path)
-        X_train, X_test, y_train, y_test = train_test_split(
-            D.data, D.targets, test_size=0.1, stratify=D.targets, random_state=1
-        )
-        train_data = ABIDE(root=data_path, transform=preprocess)
-        train_data.data = X_train
-        train_data.targets = y_train
-        test_data = ABIDE(root=data_path, transform=preprocess)
-        test_data.data = X_test
-        test_data.targets = y_test
-        train_data = get_data_splits(args, train_data)
-        loaders = {
-            "train": DataLoader(train_data, 64, shuffle=True, num_workers=2),
-            "test": DataLoader(test_data, 64, shuffle=False, num_workers=2),
-        }
-        configs = {
-            "class_names": ["non ASD", "ASD"],
-            "mask": D.get_mask(),
-        }
-    elif dataset in [
-        "food101",
-        "eurosat",
-        "sun397",
-        "ucf101",
-        "stanfordcars",
-        "flowers102",
-    ]:
-        preprocess = transforms.Compose(
-            [
-                transforms.Lambda(lambda x: x.convert("RGB")),
-                transforms.Resize((128, 128)),
-                transforms.ToTensor(),
-            ]
-        )
-        train_data = COOPLMDBDataset(
-            root=data_path, split="train", transform=preprocess
-        )
-        test_data = COOPLMDBDataset(root=data_path, split="test", transform=preprocess)
-        train_data = get_data_splits(args, train_data)
-        loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=8),
-            "test": DataLoader(test_data, 128, shuffle=False, num_workers=8),
-        }
-        configs = {
-            "class_names": refine_classnames(test_data.classes),
-            "mask": np.zeros((128, 128)),
-        }
-    elif dataset in ["dtd", "oxfordpets"]:
-        preprocess = transforms.Compose(
-            [
-                transforms.Lambda(lambda x: x.convert("RGB")),
-                transforms.Resize((128, 128)),
-                transforms.ToTensor(),
-            ]
-        )
-        train_data = COOPLMDBDataset(
-            root=data_path, split="train", transform=preprocess
-        )
-        test_data = COOPLMDBDataset(root=data_path, split="test", transform=preprocess)
-        train_data = get_data_splits(args, train_data)
-        loaders = {
-            "train": DataLoader(train_data, 64, shuffle=True, num_workers=8),
-            "test": DataLoader(test_data, 64, shuffle=False, num_workers=8),
-        }
-        configs = {
-            "class_names": refine_classnames(test_data.classes),
-            "mask": np.zeros((128, 128)),
-        }
-    else:
-        raise NotImplementedError(f"{dataset} not supported")
-    return loaders, configs
-
 
 def sample_n_shots(args, train_data):
     # Create an empty list to store the sampled indices
@@ -235,6 +61,195 @@ def get_data_splits(args, train_data):
     return trainset
 
 
+def refine_classnames(class_names):
+    for i, class_name in enumerate(class_names):
+        class_names[i] = class_name.lower().replace("_", " ").replace("-", " ")
+    return class_names
+
+
+def get_class_names_from_split(root):
+    with open(os.path.join(root, "split.json")) as f:
+        split = json.load(f)["test"]
+    idx_to_class = OrderedDict(sorted({s[-2]: s[-1] for s in split}.items()))
+    return list(idx_to_class.values())
+
+
+def prepare_expansive_data(args, dataset, data_path):
+    data_path = os.path.join(data_path, dataset)
+    if dataset == "cifar10":
+        preprocess = transforms.Compose(
+            [
+                transforms.ToTensor(),
+            ]
+        )
+        train_data = datasets.CIFAR10(
+            root=data_path, train=True, download=True, transform=preprocess
+        )
+        test_data = datasets.CIFAR10(
+            root=data_path, train=False, download=True, transform=preprocess
+        )
+        #train_data = get_data_splits(args, train_data)
+        if args.n_shot > 0:
+            train_data = sample_n_shots(args, train_data)
+        loaders = {
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
+            "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
+        }
+        configs = {
+            "class_names": refine_classnames(test_data.classes),
+            "mask": np.zeros((32, 32)),
+        }
+    elif dataset == "cifar100":
+        preprocess = transforms.Compose(
+            [
+                transforms.ToTensor(),
+            ]
+        )
+        train_data = datasets.CIFAR100(
+            root=data_path, train=True, download=True, transform=preprocess
+        )
+        test_data = datasets.CIFAR100(
+            root=data_path, train=False, download=True, transform=preprocess
+        )
+        #train_data = get_data_splits(args, train_data)
+        if args.n_shot > 0:
+            train_data = sample_n_shots(args, train_data)
+        loaders = {
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
+            "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
+        }
+        configs = {
+            "class_names": refine_classnames(test_data.classes),
+            "mask": np.zeros((32, 32)),
+        }
+    elif dataset == "gtsrb":
+        preprocess = transforms.Compose(
+            [
+                transforms.Resize((32, 32)),
+                transforms.ToTensor(),
+            ]
+        )
+        train_data = datasets.GTSRB(
+            root=data_path, split="train", download=True, transform=preprocess
+        )
+        test_data = datasets.GTSRB(
+            root=data_path, split="test", download=True, transform=preprocess
+        )
+        #train_data = get_data_splits(args, train_data)
+        if args.n_shot > 0:
+            train_data = sample_n_shots(args, train_data)
+        loaders = {
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
+            "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
+        }
+        configs = {
+            "class_names": refine_classnames(list(GTSRB_LABEL_MAP.values())),
+            "mask": np.zeros((32, 32)),
+        }
+    elif dataset == "svhn":
+        preprocess = transforms.Compose(
+            [
+                transforms.ToTensor(),
+            ]
+        )
+        train_data = datasets.SVHN(
+            root=data_path, split="train", download=True, transform=preprocess
+        )
+        test_data = datasets.SVHN(
+            root=data_path, split="test", download=True, transform=preprocess
+        )
+        #train_data = get_data_splits(args, train_data)
+        if args.n_shot > 0:
+            train_data = sample_n_shots(args, train_data)
+        loaders = {
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
+            "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
+        }
+        configs = {
+            "class_names": [f"{i}" for i in range(10)],
+            "mask": np.zeros((32, 32)),
+        }
+    elif dataset == "abide":
+        preprocess = transforms.ToTensor()
+        D = ABIDE(root=data_path)
+        X_train, X_test, y_train, y_test = train_test_split(
+            D.data, D.targets, test_size=0.1, stratify=D.targets, random_state=1
+        )
+        train_data = ABIDE(root=data_path, transform=preprocess)
+        train_data.data = X_train
+        train_data.targets = y_train
+        test_data = ABIDE(root=data_path, transform=preprocess)
+        test_data.data = X_test
+        test_data.targets = y_test
+        #train_data = get_data_splits(args, train_data)
+        if args.n_shot > 0:
+            train_data = sample_n_shots(args, train_data)
+        loaders = {
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
+            "test": DataLoader(test_data, 64, shuffle=False, num_workers=2),
+        }
+        configs = {
+            "class_names": ["non ASD", "ASD"],
+            "mask": D.get_mask(),
+        }
+    elif dataset in [
+        "food101",
+        "eurosat",
+        "sun397",
+        "ucf101",
+        "stanfordcars",
+        "flowers102",
+    ]:
+        preprocess = transforms.Compose(
+            [
+                transforms.Lambda(lambda x: x.convert("RGB")),
+                transforms.Resize((128, 128)),
+                transforms.ToTensor(),
+            ]
+        )
+        train_data = COOPLMDBDataset(
+            root=data_path, split="train", transform=preprocess
+        )
+        test_data = COOPLMDBDataset(root=data_path, split="test", transform=preprocess)
+        #train_data = get_data_splits(args, train_data)
+        if args.n_shot > 0:
+            train_data = sample_n_shots(args, train_data)
+        loaders = {
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=8),
+            "test": DataLoader(test_data, 128, shuffle=False, num_workers=8),
+        }
+        configs = {
+            "class_names": refine_classnames(test_data.classes),
+            "mask": np.zeros((128, 128)),
+        }
+    elif dataset in ["dtd", "oxfordpets"]:
+        preprocess = transforms.Compose(
+            [
+                transforms.Lambda(lambda x: x.convert("RGB")),
+                transforms.Resize((128, 128)),
+                transforms.ToTensor(),
+            ]
+        )
+        train_data = COOPLMDBDataset(
+            root=data_path, split="train", transform=preprocess
+        )
+        test_data = COOPLMDBDataset(root=data_path, split="test", transform=preprocess)
+        #train_data = get_data_splits(args, train_data)
+        if args.n_shot > 0:
+            train_data = sample_n_shots(args, train_data)
+        loaders = {
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=8),
+            "test": DataLoader(test_data, 64, shuffle=False, num_workers=8),
+        }
+        configs = {
+            "class_names": refine_classnames(test_data.classes),
+            "mask": np.zeros((128, 128)),
+        }
+    else:
+        raise NotImplementedError(f"{dataset} not supported")
+    return loaders, configs
+
+
 def prepare_additive_data(args, dataset, data_path, preprocess):
     data_path = os.path.join(data_path, dataset)
     if dataset == "cifar10":
@@ -249,7 +264,7 @@ def prepare_additive_data(args, dataset, data_path, preprocess):
         if args.n_shot > 0:
             train_data = sample_n_shots(args, train_data)
         loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=2),
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
             "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
         }
     elif dataset == "cifar100":
@@ -264,7 +279,7 @@ def prepare_additive_data(args, dataset, data_path, preprocess):
         if args.n_shot > 0:
             train_data = sample_n_shots(args, train_data)
         loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=2),
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
             "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
         }
     elif dataset == "svhn":
@@ -279,7 +294,7 @@ def prepare_additive_data(args, dataset, data_path, preprocess):
         if args.n_shot > 0:
             train_data = sample_n_shots(args, train_data)
         loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=2),
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
             "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
         }
     elif dataset in [
@@ -299,7 +314,7 @@ def prepare_additive_data(args, dataset, data_path, preprocess):
         if args.n_shot > 0:
             train_data = sample_n_shots(args, train_data)
         loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=8),
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=8),
             "test": DataLoader(test_data, 128, shuffle=False, num_workers=8),
         }
     elif dataset in ["dtd", "oxfordpets"]:
@@ -312,7 +327,7 @@ def prepare_additive_data(args, dataset, data_path, preprocess):
         if args.n_shot > 0:
             train_data = sample_n_shots(args, train_data)
         loaders = {
-            "train": DataLoader(train_data, 64, shuffle=True, num_workers=8),
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=8),
             "test": DataLoader(test_data, 64, shuffle=False, num_workers=8),
         }
     elif dataset == "gtsrb":
@@ -327,7 +342,7 @@ def prepare_additive_data(args, dataset, data_path, preprocess):
         if args.n_shot > 0:
             train_data = sample_n_shots(args, train_data)
         loaders = {
-            "train": DataLoader(train_data, 128, shuffle=True, num_workers=2),
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
             "test": DataLoader(test_data, 128, shuffle=False, num_workers=2),
         }
     elif dataset == "abide":
@@ -354,7 +369,7 @@ def prepare_additive_data(args, dataset, data_path, preprocess):
         if args.n_shot > 0:
             train_data = sample_n_shots(args, train_data)
         loaders = {
-            "train": DataLoader(train_data, 64, shuffle=True, num_workers=2),
+            "train": DataLoader(train_data, args.batch_size, shuffle=True, num_workers=2),
             "test": DataLoader(test_data, 64, shuffle=False, num_workers=2),
         }
         class_names = ["non ASD", "ASD"]
