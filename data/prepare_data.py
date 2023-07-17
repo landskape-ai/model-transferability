0e5190d6d39d320c42e900d96ddbcbf9b6d70cfb
Xa9aX ツ <mishradiganta91@gmail.com>
2023-07-17 17:13:34 -0400

    update

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -18,10 +18,10 @@ def sample_n_shots(args, train_data):
     sampled_indices = []
 
     # Iterate over the unique classes in the dataset
-    unique_classes = np.unique(np.asarray(train_data.targets))
+    unique_classes = np.unique(np.asarray(train_data.classes))
     for class_label in unique_classes:
         # Find the indices of samples belonging to the current class
-        class_indices = np.where(train_data.targets == class_label)[0]
+        class_indices = np.where(train_data.classes == class_label)[0]
 
         # shuffle the indices
         np.random.shuffle(class_indices)
