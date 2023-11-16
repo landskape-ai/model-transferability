b05f4793af27496a933275900d812582506111b0
Bharat <58414297+Bharat-Runwal@users.noreply.github.com>
2023-11-16 16:05:06 -0500

    Update train_clip.sh

--- train_clip.sh
+++ train_clip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+Download the uncompressed,2x and 4x compressed checkpoints from the [Upop (Retrieval-clip-coco)](https://github.com/sdc17/UPop/tree/main#-image-text-and-text-image-retrieval-on-the-coco-dataset-with-clip)
 wandb online
 python3 experiments/ilm_vp_clip.py \
         --sparse_chkpt /clip_chkpts/clip_large_retrieval_coco_2x_compressed.pth \
