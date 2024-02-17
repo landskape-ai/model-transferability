423aea6a9d525669841003d9a11c20c1999b0fb5
jaygala24 <jaygala24@gmail.com>
2024-02-18 00:00:32 +0530

    iterate over multiple seeds

--- run.sh
+++ run.sh
@@ -3,29 +3,34 @@ export CUDA_VISIBLE_DEVICES=0
 model=$1    # check `--model` choices in `experiments/cnn/ilm_vp.py` script
 dataset=$2  # check `--dataset` choices in `experiments/cnn/ilm_vp.py` script
 n_shots=(1 2 5 10 20 50 100 -1)
+seeds=(1 2 3)
 
 
 for n_shot in "${n_shots[@]}"; do
-    # ---------------------------------------------------------------------------
-    #                               ILM-VP experiment
-    # ---------------------------------------------------------------------------
-    python3 experiments/cnn/ilm_vp.py \
-        --model $model \
-        --n_shot $n_shot \
-        --batch_size $dataset  \
-        --dataset cifar10 \
-        --results_path results \
-        --wandb
+    for seed in "${seeds[@]}"; do
+        # ---------------------------------------------------------------------------
+        #                               ILM-VP experiment
+        # ---------------------------------------------------------------------------
+        python3 experiments/cnn/ilm_vp.py \
+            --model $model \
+            --n_shot $n_shot \
+            --seed $seed \
+            --batch_size $dataset  \
+            --dataset cifar10 \
+            --results_path results \
+            --wandb
 
-    # ---------------------------------------------------------------------------
-    #                                LP experiment
-    # ---------------------------------------------------------------------------
-    python3 experiments/cnn/linear_probing.py \
-        --model $model \
-        --n_shot $n_shot \
-        --batch_size 128 \
-        --dataset $dataset \
-        --results_path results \
-        --wandb
+        # ---------------------------------------------------------------------------
+        #                                LP experiment
+        # ---------------------------------------------------------------------------
+        python3 experiments/cnn/linear_probing.py \
+            --model $model \
+            --n_shot $n_shot \
+            --seed $seed \
+            --batch_size 128 \
+            --dataset $dataset \
+            --results_path results \
+            --wandb
 
+    done
 done
